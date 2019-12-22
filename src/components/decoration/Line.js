f357f6e9f948c68513b85a0ad8eeb6fb08418df3
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-12-22 22:06:04 +0100

    decoration

---
+++ src/components/decoration/Line.js
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+import React from "react";
+import { css, jsx } from "@emotion/core";
+
+export default () => (
+  <div
+    css={css`
+      border-bottom: 1px solid darkgray;
+      width: 50%;
+      margin: auto;
+    `}
+  ></div>
+);
