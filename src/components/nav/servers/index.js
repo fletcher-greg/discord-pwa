d09ec8094b585be2cc921066ca28c946fc5fe11a
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-12-22 22:07:37 +0100

    server styling

--- src/components/nav/servers/index.js
+++ src/components/nav/servers/index.js
@@ -1,15 +1,83 @@
 /** @jsx jsx */
 import React from "react";
 import { css, jsx } from "@emotion/core";
+import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 import Avatar from "../comps/Avatar";
-let testArr = ["Greg", "Rust", "Chin", "FCC"];
+import Line from "../../decoration/Line";
+let testArr = ["CSS", "Rust", "Chin", "FCC"];
+const UserAvatar = props => (
+  <div
+    css={css`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-bottom: 1em;
+    `}
+  >
+    <Avatar {...props}></Avatar>
+  </div>
+);
+/* const HoverIcon = () => (
+  <div
+    css={css`
+      width: 5px;
+      height: 20px;
+      background: white;
+      position: absolute;
+      left: 0;
+      top: 20px;
+    `}
+  ></div>
+); */
+const HoverIcon = styled.div`
+  width: 5px;
+  height: 20px;
+  background: white;
+  position: absolute;
+  left: 0;
+  top: 20px;
+`;
+const Wrapper = styled.div`
+  width: 100%;
+  position: relative;
+  display: grid;
+  place-items: center;
+  &:hover ${HoverIcon} {
+    background: blue;
+  }
+`;
+const ServerAvatars = () => {
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 1em;
+      `}
+    >
+      {testArr.map(person => (
+        <Wrapper>
+          >
+          <HoverIcon />
+          <Avatar text={person} />
+        </Wrapper>
+      ))}
+    </div>
+  );
+};
+
 export default () => {
   return (
-    <div>
-      {testArr.map(person => (
-        <div>{person} </div>
-      ))}
+    <div
+      css={css`
+        background: #202225;
+      `}
+    >
+      <UserAvatar text="Greg" />
+      <Line />
+      <ServerAvatars />
     </div>
   );
 };
