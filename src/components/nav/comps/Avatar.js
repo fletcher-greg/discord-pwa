505e6cb62ceb7be8b123d0bfe73272dd32e27a5f
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-12-22 22:07:10 +0100

    style avatar

--- src/components/nav/comps/Avatar.js
+++ src/components/nav/comps/Avatar.js
@@ -2,4 +2,25 @@
 import React from "react";
 import { css, jsx } from "@emotion/core";
 
-export default () => <div>Avatar</div>;
+export default ({ text }) => (
+  <div
+    css={css`
+      width: 60px;
+      height: 60px;
+      background: #393c43;
+      border-radius: 50%;
+      display: grid;
+      place-items: center;
+      font-weight: 600;
+      margin-bottom: 1em;
+      transition: border-radius 0.25s ease-in-out;
+      cursor: pointer;
+      &:hover {
+        border-radius: 20%;
+        transition: border-radius 0.3s ease-in-out;
+      }
+    `}
+  >
+    {text}
+  </div>
+);
