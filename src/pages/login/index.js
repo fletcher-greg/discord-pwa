4cfe31168da3bd08ab955de0d7dee88df4977823
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-12-23 18:51:45 +0100

    login page absolute position

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,6 @@
+/** @jsx jsx */
 import React, { useContext } from "react";
+import { css, jsx } from "@emotion/core";
 
 import useForm from "../../components/customHooks/useForm";
 import { UserContext } from "../../util/UserContext";
@@ -7,7 +9,16 @@ export default () => {
   const [values, handleChange] = useForm({ email: "", password: "" });
   const { user, setUser } = useContext(UserContext);
   return (
-    <div>
+    <div
+      css={css`
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100vh;
+        background: #9cc8f4;
+        width: 100vw;
+      `}
+    >
       <h2>{user}</h2>
       <input name="email" value={values.email} onChange={handleChange} />
     </div>
