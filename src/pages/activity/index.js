7c6fc6a32614d4a54af2df559aeba409b9312dee
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-12-24 22:31:21 +0100

    add multiple game update cards

--- src/pages/activity/index.js
+++ src/pages/activity/index.js
@@ -20,9 +20,50 @@ const Content = () => {
     margin: 0 auto;
   `;
   const rightCol = css``;
+  const leftCol = css``;
+
+  let news = [
+    {
+      title: "Looking good!",
+      sub:
+        "Coming to Steam, Xbox One and Wegame next week on December 12th, we will be bringing Hook, Line and Inker to the live servers along with this year's Winter's Feast event.",
+      time: "A week ago",
+      url: "steamcommunity"
+    },
+    {
+      title: "That's a wrap!",
+      sub:
+        "Coming to Steam, Xbox One and Wegame next week on December 12th, we will be bringing Hook, Line and Inker to the live servers along with this year's Winter's Feast event.",
+      time: "A week ago",
+      url: "steamcommunity"
+    },
+    {
+      title: "Looking good!",
+      sub: "Do whatever it takes, but most importantly, Don't Starve.",
+      time: "A year ago",
+      url: "steamcommunity"
+    },
+    {
+      title: "Patching the bugs",
+      sub: "We're sorry about the bugs, hopefully this patch will squash them!",
+      time: "2 hours ago",
+      url: "steamcommunity"
+    },
+    {
+      title: "Loot boxes",
+      sub:
+        "We've given in.  The game is full of loot boxes.  It's pay to win, or it's not worth it.  Maybe it's not worth it...",
+      time: "1 second ago",
+      url: "steamcommunity"
+    }
+  ];
   return (
     <div css={contentstyle}>
-      <GameUpdate />
+      <div css={leftCol}>
+        {news.map(card => (
+          <GameUpdate />
+        ))}
+      </div>
       <div css={rightCol}>
         <QLauncher />
         <NowPlaying />
