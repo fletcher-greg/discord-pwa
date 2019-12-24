fc4e899c9fb2bbd590549b37c8bcff6488451767
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-12-24 18:45:18 +0100

    add column info

--- src/pages/friends/index.js
+++ src/pages/friends/index.js
@@ -8,12 +8,24 @@ import FriendList from "./comps/FriendList";
 const pageStyle = css`
   background: #36393f;
 `;
-
+const ColumnInfo = () => {
+  const P = ({ text }) => <p>{text}</p>;
+  const info = ["NAME", "STATUS"];
+  const columnStyle = css``;
+  return (
+    <div css={columnStyle}>
+      {info.map(n => (
+        <P text={n} />
+      ))}
+    </div>
+  );
+};
 export default () => {
   const [tog, setTog] = useState(true);
   return (
     <div css={pageStyle}>
       <TopBar setTog={setTog} page="Friends" />
+      <ColumnInfo />
       <FriendList tog={tog} />
     </div>
   );
