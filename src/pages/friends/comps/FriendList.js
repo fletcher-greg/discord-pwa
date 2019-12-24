4163bd2e5de62dd50892bc9ce263ae53d78f296f
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-12-24 18:34:33 +0100

    add react-spring animation

--- src/pages/friends/comps/FriendList.js
+++ src/pages/friends/comps/FriendList.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { css, jsx } from "@emotion/core";
-// import { useSpring, animated } from "react-spring";
+import { useTrail, animated } from "react-spring";
 // LOCAL IMPORTS
 import { Avatar, Name } from "../../../components/nav/comps/DirectMessages";
 const fDataStyle = css`
@@ -29,12 +29,65 @@ const MutualServers = () => (
   <div
     css={css`
       width: 250px;
+      display: flex;
+      align-items: center;
     `}
   >
     <Avatar />
   </div>
 );
-export default props => {
+
+const freunden = ["Sally", "Chewie", "Hal", "Chris"];
+
+export default ({ tog }) => {
+  const [trail, set] = useTrail(freunden.length, () => ({
+    config: { duration: 400 },
+
+    from: { opacity: 0, height: 0, display: "none" },
+    to: {
+      opacity: 1,
+      height: 62,
+      display: "flex"
+    }
+  }));
+  console.log("again");
+  useEffect(() => {
+    console.log("hi");
+    async function test() {
+      await set({ opacity: tog ? 1 : 0, height: tog ? 62 : 0 });
+
+      console.log("finished");
+    }
+    test();
+  }, [tog]);
+  return (
+    <div>
+      {trail.map((props, index) => (
+        <animated.div
+          // onClick={() => setTog(t => !t)}
+          style={props}
+          css={css`
+            display: flex;
+            flex-direction: row;
+
+            border-bottom: 1px solid #2f3136;
+
+            cursor: pointer;
+            &:hover {
+              background-color: #393c43;
+              border: none;
+            }
+          `}
+        >
+          <FriendData name={freunden[index]} />
+          <Status />
+          <MutualServers />
+        </animated.div>
+      ))}
+    </div>
+  );
+};
+const FriendList = props => {
   return (
     <div
       // onClick={() => setTog(t => !t)}
--- src/pages/friends/index.js
+++ src/pages/friends/index.js
@@ -3,17 +3,18 @@ import React, { useState } from "react";
 import { css, jsx } from "@emotion/core";
 
 // LOCAL IMPORTS
-import TopBar from "../../components/topBar";
+import TopBar from "./comps/TopBar";
 import FriendList from "./comps/FriendList";
 const pageStyle = css`
   background: #36393f;
 `;
 
 export default () => {
+  const [tog, setTog] = useState(true);
   return (
     <div css={pageStyle}>
-      <TopBar page="Friends" />
-      <FriendsWrapper />
+      <TopBar setTog={setTog} page="Friends" />
+      <FriendList tog={tog} />
     </div>
   );
 };
